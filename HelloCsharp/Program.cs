119919df3bc23810351b0440bef2f5a138e1d30d
alexpagani <alexpaganibr@gmail.com>
2022-03-31 14:12:44 +0200

    Fine Esercizio Mesi dell'anno

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//Inizio programma
+
+Console.WriteLine("Benvenuto");
+Console.WriteLine("Ecco i nostri mesi dell'anno:");
+Console.WriteLine("- Gennaio");
+Console.WriteLine("- Febbraio");
+Console.WriteLine("- Marzo");
+Console.WriteLine("- Aprile");
+Console.WriteLine("- Maggio");
+Console.WriteLine("- Giugno");
+Console.WriteLine("- Luglio");
+Console.WriteLine("- Agosto");
+Console.WriteLine("- Settembre");
+Console.WriteLine("- Ottobre");
+Console.WriteLine("- Novembre");
+Console.WriteLine("- Dicembre");
+
+string mesePreferito;
+
+Console.Write("Inserisci il tuo mese preferito: ");
+mesePreferito = Console.ReadLine();
+
+Console.Write("Questo è il tuo mese preferito: ");
+Console.WriteLine(mesePreferito);
